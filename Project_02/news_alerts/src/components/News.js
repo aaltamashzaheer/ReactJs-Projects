9c36b29b82e7cfff88a7cccf0301cc777f33a232
Altamash Zaheer <aaltamashzaheer@gmail.com>
2023-06-25 13:10:14 -0700

    Added Category Name in Title

--- Project_02/news_alerts/src/components/News.js
+++ Project_02/news_alerts/src/components/News.js
@@ -14,13 +14,19 @@ export class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   };
-  constructor() {
-    super();
+  CaptalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `WebTimes - ${this.CaptalizeFirstLetter(
+      this.props.category
+    )}`;
   }
   async updateNews() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7bc6ddd0cb5a4dfaa590694566477f20&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -42,7 +48,7 @@ export class News extends Component {
     //     this.state.page + 1 >
     //     Math.ceil(this.state.totalResults / this.props.pageSize)
     //   )
-    // ) 
+    // )
     //{
     //   let url = `https://newsapi.org/v2/top-headlines?country=${
     //     this.props.country
@@ -61,7 +67,7 @@ export class News extends Component {
     //   });
     // }
     this.setState({ page: this.state.page + 1 });
-    this.updateNews()
+    this.updateNews();
   };
 
   handlePreviousButton = async () => {
@@ -72,17 +78,19 @@ export class News extends Component {
     // }&apiKey=7bc6ddd0cb5a4dfaa590694566477f20&page=${
     //   this.state.page - 1
 
-    
     this.setState({ page: this.state.page - 1 });
-    this.updateNews()
+    this.updateNews();
   };
   render() {
     return (
       <div>
         <div className="container my-3"></div>
-        <h2 className="text-center">Today's Top Headlines are:</h2>
+        <h2 className="text-center">
+          Web Times - Today's {this.CaptalizeFirstLetter(this.props.category)}{" "}
+          Headlines are:
+        </h2>
         {this.state.loading && <Spinner />}
-        <div className="row">
+        <div className="row my-3">
           {!this.state.loading &&
             this.state.articles.map((element) => {
               return (
